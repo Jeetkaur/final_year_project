9e60c633e2f2dff1c1b393de1cf4d7238c30c9a8
jeetkaur <simran.soft07@gmail.com>
2017-08-03 12:30:49 -0700

    spy name

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 print "let's get started"
 spy_name=raw_input("provide your name here:")
 spy_salutation=raw_input("what should we call  you:")
+if len(spy_name)> 0:
+#always remember indendation one tab either 4 space
+    new_message=  "yay! name is not empty"
+    print new_message
+
 spy_name = spy_salutation+" "+spy_name
-length=len(spy_name)
-
-
-
 print ' welcome ' ""   + spy_name + " Glad to have you back with "
