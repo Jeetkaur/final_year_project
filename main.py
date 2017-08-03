c81f1c6657b36a09970d8074d5867c33af8428d1
jeetkaur <simran.soft07@gmail.com>
2017-08-03 11:42:54 -0700

    main program

---
+++ main.py
@@ -0,0 +1,9 @@
+print "let's get started"
+spy_name=raw_input("provide your name here:")
+spy_salutation=raw_input("what should we call  you:")
+spy_name = spy_salutation+" "+spy_name
+length=len(spy_name)
+
+
+
+print ' welcome ' ""   + spy_name + " Glad to have you back with "
