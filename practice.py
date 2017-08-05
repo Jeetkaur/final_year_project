12c38153e57f07b19419402288caf65e90c04325
jeetkaur <simran.soft07@gmail.com>
2017-08-05 11:51:22 -0700

    my caluclator program using if

--- practice.py
+++ practice.py
@@ -1,5 +1,25 @@
 print "caluclator"
-first_no=int(raw_input("enter first no:"))
-second_no=int(raw_input("enter second no:"))
-sum=first_no + second_no
-print sum
+choice=raw_input("Enter your choice what do you want to perform: +  for Addition - for for subtraction  * for multiplication / for division");
+print choice
+num1=int(raw_input("enter first no."))
+num2=int(raw_input("enter second no."))
+
+
+if(choice=='+'):
+    print num1+num2
+elif(choice=='-'):
+    if(num1<num2):
+        print"num1 should be greater than num2"
+    num3=num1
+    num1=num2
+    num2=num3
+    print num1-num2
+elif(choice=='*'):
+    print num1*num2
+elif(choice=='/'):
+    if(num1<0):
+        print "enter positive value"
+
+    print num1/num2
+else:
+    print "plz choose a correct choice"
