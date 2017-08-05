a31c3da6dd6ef7bd6b509b3409c315fac3bb0003
jeetkaur <simran.soft07@gmail.com>
2017-08-05 12:02:37 -0700

    whether no. is -ve or +ve or zero

---
+++ check.py
@@ -0,0 +1,8 @@
+print"to check whether a no. is negative ,positve and zero"
+num=int(raw_input("enter a no."))
+if(num<0):
+    print"num is negative"
+elif(num>0):
+    print"num is postitve"
+else:
+    print "num is zero"
