7f3bdf2e50765d19d540940f3ef726d6b395f901
jeetkaur <simran.soft07@gmail.com>
2017-08-09 12:35:01 -0700

    reverse string

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 print "Let's get started"
 spy_name=raw_input("provide your name here: ")
 if len(spy_name)> 0:
-    print "Alright " + spy_name + " I would like to know something about u before we proceed further"
+    spy_age=0
+    spy_rating=0.0
+    spy_is_online=False
     spy_salutation = raw_input("what should we call  you: ")
+    #spy_age=raw_input("Enter your age: ")
+    #print type(spy_age)            #it always returns string
+    spy_age = int(raw_input("Enter your age: "))
+    print type(spy_age)  # it returns int
+    if spy_age > 12 and spy_age <60:
+        spy_rating=float(raw_input("Enter spy rating: "))
+    if spy_rating>4.5:
+        print "you have a great spy_rating"
+    elif spy_rating>=3.5 and spy_rating <= 4.5:
+        print "you are one of the graet one"
+    elif spy_rating>=2.5 and spy_rating <=3.5:
+        print"you need to do more hardwork"
+    else:
+        print "You are not eligible to be spy"
+
+    spy_is_online=(raw_input("Spy is online or not :"))
     spy_name = spy_salutation+" "+spy_name
     print 'welcome ' ""   + spy_name + " Glad to have you back with "
+    print "Alright " + spy_name + " I would like to know something about u before we proceed further"
 else:
     print "A spy needs to have a valid name, please try againn"
-spy_age=raw_input("")
 
--- reversestring.py
+++ reversestring.py
@@ -1,4 +1,4 @@
 print "reverse the name"
-first_name=raw_input("enter your first name")
-last_name=raw_input("enter your last name")
-print  first_name.reverse()
+#first_name=raw_input("enter your first name: ")
+#last_name=raw_input("enter your last name :")
+print  'jeet'[::-1]+" "+ 'simran'[::-1]
